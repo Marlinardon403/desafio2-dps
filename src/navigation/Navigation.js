223910a027faf9127261e9355fae0af4373fc1af
Marlin Ardon <marlinardon403@outlook.es>
2021-10-09 13:34:31 -0600

    Inicio del repositorio

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import {createBottomNavigator} from '@react-navigation/stack';
+import HomeStack from './HomeStack';
+import AboutStack from './AboutStack';
+import ContactStack from './ConctactStack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+export default function Navigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="home" component={HomeStack} options={{title: 'Home'}} />
+      <Tab.Screen name="about" component={AboutStack} options={{title: 'About'}} />
+      <Tab.Screen name="contact" component={ContactStack} options={{title: 'Contact'}}/>
+    </Tab.Navigator>
+  );
+}
